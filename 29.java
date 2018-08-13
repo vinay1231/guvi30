a81fa9ece5d8452b166752904eb17410c48a08e8
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-08-13 11:47:40 +0530

    Create 29.java

---
+++ 29.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	     Scanner s = new Scanner (System.in);
+
+	     int t = s.nextInt();
+
+	      int k = (t/60); 
+
+	      int z = (t%60);
+
+	      System.out.println(k+" "+z);
+
+	}
+
+}
